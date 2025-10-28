cbee9e3e7ac9427ab3f405976e469c3747d7256f
wilgotlucaci <wilgot.lucaci@iths.se>
2025-10-28 09:34:53 +0100

    ex 1

--- Exercises/0.py
+++ Exercises/0.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import numpy as np
+df_kommuner = pd.DataFrame({
+    "Kommuner" : list(("Malmö","Stockholm", "Uppsala", "Göteborg" )),
+    "Population": np.array((347949, 975551, 233839, 583056))},
+    index= ["1", "2", "3", "4"])
 
 
-data = dict(AI = 25, Net = 33, JAVA = 32)
+print(df_kommuner["Kommuner"])
 
-series_program = pd.Series(data)
-print(series_program)
+print(df_kommuner.loc["4"])
+
+städer = df_kommuner[df_kommuner["Population"]]
+
+städer_sorterad = städer.sort_values(by="Population", ascending=False)
+print(städer_sorterad)
